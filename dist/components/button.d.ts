0371c70e37900f36b79106cf9dacee627227fa91
mula <mularahman57@gmail.com>
2024-10-05 07:08:11 +0800

    test

--- dist/components/button.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-type ButtonProps = {
-    variant?: 'primary' | 'danger' | 'success';
-    children: React.ReactNode;
-    className?: string;
-    onClick?: () => void;
-};
-declare const Button: React.FC<ButtonProps>;
-export default Button;
