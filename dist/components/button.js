92d7e50c33f8b30fe66756e7a0fd253c51f75381
mula <mularahman57@gmail.com>
2024-10-05 07:20:04 +0800

    test

---
+++ dist/components/button.js
@@ -0,0 +1,14 @@
+import React from "react";
+const variantClasses = {
+    primary: 'bg-blue-500 text-white hover:bg-blue-700',
+    secondary: 'bg-gray-500 text-white hover:bg-gray-700',
+    danger: 'bg-red-500 text-white hover:bg-red-700',
+    outline: 'border border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white',
+    success: 'bg-green-500 text-white hover:bg-green-700',
+};
+const Button = ({ label = 'Button', className = '', onClick, variant = 'primary' }) => {
+    const classes = `${variantClasses[variant]} px-4 py-2 rounded ${className}`;
+    return (React.createElement(React.Fragment, null,
+        React.createElement("button", { onClick: onClick, className: classes }, label)));
+};
+export default Button;
